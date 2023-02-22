bd27f8235f72d086e20f6ee71d62c7680ce6de5a
Jegatheesh <jega7354@gmail.com>
2023-02-22 19:50:27 +0530

    software development and value

--- src/components/Sections/SoftwareDevelopment.jsx
+++ src/components/Sections/SoftwareDevelopment.jsx
@@ -7,16 +7,23 @@ function SoftwareDevelopment() {
         <section id="our-values">
             <div className='p-4 container'>
                 <HeaderInfo>
-                    <h1 className="font40 extraBold textCenter">The Value we Bring to the Table</h1>
+                    <h1 className="font40 extraBold textCenter">Software Development</h1>
                 </HeaderInfo>
+                <SubTitle>
+                    <h3 className='text-center'>“Whether you want to enhance an
+existing application
+or develop a new one, Oryx Space
+caters to all your business needs.”
+</h3>
+                </SubTitle>
                 <div className="cardscontainer">
                 {Data.map((development) =>(
-                    <div className="col-xs-12 col-sm-4 col-md-4 col-lg-4 py-4">
+                    <div className="col-xs-12 col-sm-4 col-md-4 col-lg-4 py-4 card-container">
                         <div className="software-card">
                             <img className='iconbox' data-aos="fade-right" data-aos-duration="2000" src={require('../../assets/img/development/'+development.image)} alt={development.title}>
                             </img>
                             <p className='py-4'>
-                                Accelerating Businesses in the Digital Age
+                                {development.title}
                             </p>
                         </div>
                     </div>
@@ -30,6 +37,12 @@ function SoftwareDevelopment() {
 export default SoftwareDevelopment;
 
 const HeaderInfo = styled.div`
+  @media (max-width: 860px) {
+    text-align: center;
+  }
+`;
+const SubTitle = styled.div`
+margin-top:16px;
   @media (max-width: 860px) {
     text-align: center;
   }
